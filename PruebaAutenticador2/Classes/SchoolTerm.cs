f90f0d4476028a1e528cd2288019356af38f5f61
Rogelio Ceballos Castillo <rogelioceballos218@gmail.com>
2026-03-20 18:11:57 -0600

    Archivos launchSettings.json, appsettings.Development.json y docker-compose.yaml documentados; publish.yaml, SchoolTerm.cs, View_Calendar.razor, View_Graphics.razor, Calendar.css y GraphSearch.css modificados

--- PruebaAutenticador2/Classes/SchoolTerm.cs
+++ PruebaAutenticador2/Classes/SchoolTerm.cs
@@ -5,7 +5,7 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Nombre { get; set; } = null!;
-        public DateTimeOffset FechaInicio { get; set; }
-        public DateTimeOffset FechaFin { get; set; }
+        public DateTimeOffset FechaInicio { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset FechaFin { get; set; } = DateTimeOffset.Now;
     }
 }
